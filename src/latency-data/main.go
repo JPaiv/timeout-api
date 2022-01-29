7ccf331a09953d75530dcf9d66f30ab16a540b8b
JPaiv <juho.paivarinta@outlook.com>
2022-01-29 22:20:03 +0200

    Add download from s3 function-

--- src/latency-data/main.go
+++ src/latency-data/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
+	"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 // Response is of type APIGatewayProxyResponse since we're leveraging the
@@ -16,28 +20,30 @@ import (
 type Response events.APIGatewayProxyResponse
 
 // Handler is our lambda handler invoked by the `lambda.Start` function call
-func Handler(ctx context.Context) (Response, error) {
-	var buf bytes.Buffer
-
-	body, err := json.Marshal(map[string]interface{}{
-		"message": "Go Serverless v1.0! Your function executed successfully!",
-	})
+func Handler(ctx context.Context, s3Event events.S3Event) {
+	item := "api_latencies.json"
+	file, err := os.Create(item)
 	if err != nil {
-		return Response{StatusCode: 404}, err
-	}
-	json.HTMLEscape(&buf, body)
-
-	resp := Response{
-		StatusCode:      200,
-		IsBase64Encoded: false,
-		Body:            buf.String(),
-		Headers: map[string]string{
-			"Content-Type":           "application/json",
-			"X-MyCompany-Func-Reply": "hello-handler",
-		},
+		fmt.Println("Unable to open file %q, %v", item, err)
 	}
 
-	return resp, nil
+	defer file.Close()
+
+	for _, record := range s3Event.Records {
+		s3 := record.S3
+		fmt.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3.Bucket.Name, s3.Object.Key)
+	}
+	sess, _ := session.NewSession(&aws.Config{Region: aws.String("us-east-1")})
+	downloader := s3manager.NewDownloader(sess)
+	numBytes, err := downloader.Download("api_latencies.json",
+		&s3.GetObjectInput{
+			Bucket: aws.String(s3.Bucket.Name),
+			Key:    aws.String(s3.Object.Key),
+		})
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Downloaded", file.Name(), numBytes, "bytes")
 }
 
 func main() {
