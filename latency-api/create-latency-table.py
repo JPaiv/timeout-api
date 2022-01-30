dd391275a191fe0d098f92f895013ecf93f83995
JPaiv <juho.paivarinta@outlook.com>
2022-01-30 11:26:00 +0200

    Plz work.

--- latency-api/create-latency-table.py
+++ latency-api/create-latency-table.py
@@ -16,16 +16,18 @@ def handler(event, context):
     bucket_name = event['Records'][0]['s3']['bucket']['name']
     file_key = event['Records'][0]['s3']['object']['key']
     logger.info('Reading {} from {}'.format(file_key, bucket_name))
+
     # s3.get_object(Bucket=bucket_name, Key=file_key)
-    s3.Bucket(bucket_name).download_file(
-        file_key, "latencies.json")
-    with open("latencies.json") as source_file:
-        source_file = json.loads(source_file)
-        for key, value in source_file.items():
-            latency_data = {}
-            latency_data["id"] = key
-            latency_data["latency"] = value
-            _write_to_dynamo(latency_data)
+    content_object = s3.Object(bucket_name, file_key)
+    file_content = content_object.get()['Body'].read().decode('utf-8')
+    json_content = json.loads(file_content)
+    # with open("latencies.json") as source_file:
+    #     source_file = json.loads(source_file)
+    for key, value in json_content.items():
+        latency_data = {}
+        latency_data["id"] = key
+        latency_data["latency"] = value
+        _write_to_dynamo(latency_data)
 
 
 # def _create_dynamo_latency_object(source_data: dict) -> dict:
