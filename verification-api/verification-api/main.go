be3df8301577890e7d1199049867786e1a9561ac
JPaiv <juho.paivarinta@outlook.com>
2022-01-30 22:50:41 +0200

    Parse record.

--- verification-api/verification-api/main.go
+++ verification-api/verification-api/main.go
@@ -9,7 +9,6 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
@@ -25,25 +24,24 @@ func handler(ctx context.Context, s3Event events.S3Event) {
 	for _, record := range s3Event.Records {
 		s3 := record.S3
 		fmt.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3.Bucket.Name, s3.Object.Key)
-	}
-	file, err := os.Create(s3.Object.Key)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer file.Close()
+		file, err := os.Create(s3.Object.Key)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer file.Close()
 
-	sess, _ := session.NewSession(&aws.Config{Region: aws.String("eu-west-1")})
-	downloader := s3manager.NewDownloader(sess)
-	numBytes, err := downloader.Download(file,
-		&s3.GetObjectInput{
-			Bucket: aws.String(s3.Bucket.Name),
-			Key:    aws.String(s3.Object.Key),
-		})
-	if err != nil {
-		fmt.Println(err)
+		sess, _ := session.NewSession(&aws.Config{Region: aws.String("eu-west-1")})
+		downloader := s3manager.NewDownloader(sess)
+		numBytes, err := downloader.Download(file,
+			&s3.GetObjectInput{
+				Bucket: aws.String(s3.Bucket.Name),
+				Key:    aws.String(s3.Object.Key),
+			})
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Printf("File: %s", numBytes)
 	}
-	fmt.Printf("File: %s", numBytes)
-
 }
 
 func main() {
