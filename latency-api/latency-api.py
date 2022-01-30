991dd081dd8ffe7dd0dac2442e1a3d606750ed0f
JPaiv <juho.paivarinta@outlook.com>
2022-01-30 15:00:54 +0200

    Remove type conversion.

--- latency-api/latency-api.py
+++ latency-api/latency-api.py
@@ -42,7 +42,7 @@ def _query_dynamo_by_id(body: dict) -> dict:
     logging.info(item_retrieved_from_db)
     if item_retrieved_from_db:
         body["verified"] = True
-        wait(int(item_retrieved_from_db["latency"]))
+        wait(item_retrieved_from_db["latency"])
     else:
         body["verified"] = False
     return body
