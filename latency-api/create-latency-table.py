0e618a9f35b003e5bbb0526fe82277dbb7f3b698
JPaiv <juho.paivarinta@outlook.com>
2022-01-30 10:51:42 +0200

    Add s3 boto resource.

--- latency-api/create-latency-table.py
+++ latency-api/create-latency-table.py
@@ -12,7 +12,7 @@ logger.setLevel(logging.INFO)
 def handler(event, context):
     logger.info(json.dumps(event))
 
-    s3 = boto3.client('s3')
+    s3 = boto3.resource('s3')
     bucket_name = event['Records'][0]['s3']['bucket']['name']
     file_key = event['Records'][0]['s3']['object']['key']
     logger.info('Reading {} from {}'.format(file_key, bucket_name))
