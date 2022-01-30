db82bdf9ef82a8273a078aab9420e3bad73b5dd4
JPaiv <juho.paivarinta@outlook.com>
2022-01-30 22:59:09 +0200

    Add s3 depency.

--- verification-api/verification-api/main.go
+++ verification-api/verification-api/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
@@ -22,8 +23,8 @@ type Response events.APIGatewayProxyResponse
 func handler(ctx context.Context, s3Event events.S3Event) {
 	fmt.Printf("File: %s", s3Event)
 	for _, record := range s3Event.Records {
-		s3 := record.S3
-		fmt.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3.Bucket.Name, s3.Object.Key)
+		s3_record := record.S3
+		fmt.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3_record.Bucket.Name, s3_record.Object.Key)
 		file, err := os.Create(s3.Object.Key)
 		if err != nil {
 			fmt.Println(err)
@@ -34,8 +35,8 @@ func handler(ctx context.Context, s3Event events.S3Event) {
 		downloader := s3manager.NewDownloader(sess)
 		numBytes, err := downloader.Download(file,
 			&s3.GetObjectInput{
-				Bucket: aws.String(s3.Bucket.Name),
-				Key:    aws.String(s3.Object.Key),
+				Bucket: aws.String(s3_record.Bucket.Name),
+				Key:    aws.String(s3_record.Object.Key),
 			})
 		if err != nil {
 			fmt.Println(err)
