4cfd672bc87e571f3c9f4cc674b0c3476de50102
JPaiv <juho.paivarinta@outlook.com>
2022-01-30 12:01:32 +0200

    Add event parse.

--- latency-api/latency-api.py
+++ latency-api/latency-api.py
@@ -3,11 +3,32 @@ import boto3
 import json
 import logging
 import os
+from requests.models import Response
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-def handler(event, context):
-    logger.info(json.dumps(event))
-    event = event["body"]
+def handler(event: dict, context: dict) -> Response:
+    body: dict = _get_body_from_event(event)
+    logger.info(json.dumps(body))
+
+
+def _get_body_from_event(event: dict):
+    body: dict = event["body"]
+    body: dict = json.loads(body)
+    logging.info(body)
+    return body
+
+
+def _create_response(body):
+    if "email_status" in body:
+        return {
+            "status": 400,
+            "content": json.dumps(body)
+        }
+    else:
+        return {
+            "status": 200,
+            "content": json.dumps(body)
+        }
