6e317a8fb71d8abe93e44a838203aef25c92ab98
JPaiv <juho.paivarinta@outlook.com>
2022-01-30 09:25:40 +0200

    Deploy download from s3 based on event.

--- latency-api/create-latency-table.py
+++ latency-api/create-latency-table.py
@@ -1,14 +1,16 @@
+import boto3
 import json
 import logging
 
 # Initialize you log configuration using the base class
-logging.basicConfig(level=logging.INFO)
-
-# Retrieve the logger instance
 logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 def handler(event, context):
     logger.info(json.dumps(event))
 
-    logger.info("Homma toimii.")
+    s3 = boto3.client('s3')
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    file_key = event['Records'][0]['s3']['object']['key']
+    logger.info('Reading {} from {}'.format(file_key, bucket_name))
