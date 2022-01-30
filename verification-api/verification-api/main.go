45091c46b20a4fbda23e1bf6d18302e6f66ded78
JPaiv <juho.paivarinta@outlook.com>
2022-01-30 22:41:01 +0200

    Add more logging.

--- verification-api/verification-api/main.go
+++ verification-api/verification-api/main.go
@@ -21,6 +21,7 @@ type Response events.APIGatewayProxyResponse
 
 // Handler is our lambda handler invoked by the `lambda.Start` function call
 func handler(ctx context.Context, s3Event events.S3Event) {
+	fmt.Printf("File: %s", s3Event)
 	for _, record := range s3Event.Records {
 		s3 := record.S3
 		fmt.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3.Bucket.Name, s3.Object.Key)
