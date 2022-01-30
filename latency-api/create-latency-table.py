89e263a246b32a32cf6d61b72fcd927d493f933b
JPaiv <juho.paivarinta@outlook.com>
2022-01-30 11:46:23 +0200

    Add verification api.

--- latency-api/create-latency-table.py
+++ latency-api/create-latency-table.py
@@ -4,7 +4,6 @@ import json
 import logging
 import os
 
-# Initialize you log configuration using the base class
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
@@ -17,28 +16,16 @@ def handler(event, context):
     file_key = event['Records'][0]['s3']['object']['key']
     logger.info('Reading {} from {}'.format(file_key, bucket_name))
 
-    # s3.get_object(Bucket=bucket_name, Key=file_key)
     content_object = s3.Object(bucket_name, file_key)
     file_content = content_object.get()['Body'].read().decode('utf-8')
     json_content = json.loads(file_content)
-    # with open("latencies.json") as source_file:
-    #     source_file = json.loads(source_file)
+    logger.info(json.dumps(json_content))
     for key, value in json_content.items():
-        logger.info(key)
-        logger.info(value)
         latency_data = {}
         latency_data["id"] = key
         latency_data["latency"] = value
         logger.info(json.dumps(latency_data))
         _write_to_dynamo(latency_data)
-
-
-# def _create_dynamo_latency_object(source_data: dict) -> dict:
-#     latency_data = {}
-#     for key, value in source_data.items():
-#         latency_data["id"] = key
-#         latency_data["latency"] = value
-#     return latency_data
 
 
 def _write_to_dynamo(latency_data: dict):
