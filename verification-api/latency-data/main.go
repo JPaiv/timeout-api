2b9a9f2a31888fda109b0a0a137dbe7aeb405ea2
JPaiv <juho.paivarinta@outlook.com>
2022-01-30 02:34:58 +0200

    Refactor.

---
+++ verification-api/latency-data/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
+
+// Response is of type APIGatewayProxyResponse since we're leveraging the
+// AWS Lambda Proxy Request functionality (default behavior)
+//
+// https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
+type Response events.APIGatewayProxyResponse
+
+// Handler is our lambda handler invoked by the `lambda.Start` function call
+func Handler(ctx context.Context, s3Event events.S3Event) {
+	log.Printf("EVENT: %s", s3Event)
+	bucket := "dev-timeout-api-transaction-latencies"
+	item := "hello-world.txt"
+
+	file, err := os.Create(item)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	sess, _ := session.NewSession(&aws.Config{Region: aws.String("eu-west-1")})
+	downloader := s3manager.NewDownloader(sess)
+	numBytes, err := downloader.Download(file,
+		&s3.GetObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    aws.String(item),
+		})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Downloaded", file.Name(), numBytes, "bytes")
+}
+
+func main() {
+	lambda.Start(Handler)
+}
