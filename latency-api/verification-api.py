5346b60da37e74bf8f96e11dfabe4ec750fb6791
JPaiv <juho.paivarinta@outlook.com>
2022-01-31 05:30:22 +0200

    Remove all.

--- latency-api/verification-api.py
+++ latency-api/verification-api.py
@@ -2,24 +2,28 @@ import csv
 import boto3
 import json
 import logging
-import os
-
+import requests
+import time
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
 def handler(event, context):
     logger.info(json.dumps(event))
-
     s3_client = boto3.client('s3')
     bucket_name = event['Records'][0]['s3']['bucket']['name']
     file_key = event['Records'][0]['s3']['object']['key']
     logger.info('Reading {} from {}'.format(file_key, bucket_name))
-
-    s3_client.download_file(bucket_name, file_key, file_key)
-    transactions = _read_source_csv_for_financial_transactions(file_key)
+    local_file_name = '/tmp/' + file_key
+    s3_client.download_file(bucket_name, file_key, local_file_name)
+    transactions = _read_source_csv_for_financial_transactions(local_file_name)
     sorted_transactions = _sort_by_amount(transactions)
-    logger.info(json.dumps(sorted_transactions))
+    start_time = time.time()
+    succesful_verifications = []
+    for transaction in sorted_transactions:
+        verified_transaction = _verify_transaction(transaction)
+        if verified_transaction["verified"]:
+            succesful_verifications.append(verified_transaction)
 
 
 def _read_source_csv_for_financial_transactions(file_key) -> list:
@@ -42,5 +46,8 @@ def _sort_by_amount(transactions: list) -> list:
     return sorted_transactions
 
 
-def _verify_transaction(transactiond: dict) -> dict:
-    pass
+def _verify_transaction(transaction: dict) -> dict:
+    response = requests.request(
+        "GET", "https://a6z1z5aa46.execute-api.eu-west-1.amazonaws.com/verifyTransaction", data=json.dumps(transaction), timeout=60)
+    logger.info(type(response.json()))
+    return json.loads(transaction)
