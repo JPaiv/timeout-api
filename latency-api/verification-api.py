7c1b6a3458be7477f8956e53eb8d7686c66f76d5
JPaiv <juho.paivarinta@outlook.com>
2022-01-31 19:57:46 +0200

    Add more logging.

--- latency-api/verification-api.py
+++ latency-api/verification-api.py
@@ -27,7 +27,7 @@ def handler(event, context):
     for index, transaction in enumerate(sorted_transactions):
         verified_transaction = _verify_transaction(transaction)
         logger.info(verified_transaction)
-        if verified_transaction["verified"]:
+        if verified_transaction["content"]["verified"]:
             succesful_verifications.append(verified_transaction)
             del sorted_transactions[index]
 
